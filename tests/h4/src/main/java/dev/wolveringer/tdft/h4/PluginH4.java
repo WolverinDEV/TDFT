58ce3deb21a5abae48e1e66d127b54f0d4c39489
WolverinDEV <git@mcgalaxy.de>
2019-11-09 00:13:17 +0100

    Initializing implementation for h4

---
+++ tests/h4/src/main/java/dev/wolveringer/tdft/h4/PluginH4.java
@@ -0,0 +1,20 @@
+package dev.wolveringer.tdft.h4;
+
+import dev.wolveringer.tdft.plugin.Plugin;
+
+public class PluginH4 extends Plugin {
+    @Override
+    public String getName() {
+        return "Test H4";
+    }
+
+    @Override
+    public int getVersion() {
+        return 1;
+    }
+
+    @Override
+    public void onEnable() {
+        //TODO
+    }
+}
